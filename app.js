da30ac5d0d5ba29e433e7950d8d1cc7efd286c8d
shedrack-prog <usheddy07@gmail.com>
2023-05-25 19:50:10 +0100

    removed access-control-allow-headers

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, multipart/form-data');
   next();
 });
 app.use(express.json());
