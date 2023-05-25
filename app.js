6210a7f5d18591e27885ba42538785dc83ac6a5e
shedrack-prog <usheddy07@gmail.com>
2023-05-25 17:49:57 +0100

    added a cors header

--- app.js
+++ app.js
@@ -6,24 +6,27 @@ const connectDB = require('./db/connectDB.js');
 const authRoutes = require('./routes/authRoutes.js');
 const postRoutes = require('./routes/postRoutes.js');
 // imports>>>>>>>>>>>>>>>>>>>
-const cors = require('cors');
+// const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
 // middlewares>>>>>>>>>>>>>>>>>>>>
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PUT, PATCH, DELETE'
-//   );
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://mern-blog-frontend-eosin.vercel.app'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 app.use(
   fileUpload({
